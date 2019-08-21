238a5967c2f215b75aad1a17b2fc024102a4a1b6
HORI Taisuke <tahori@yahoo-corp.jp>
2019-08-21 21:47:35 +0900

    Initial Commit

---
+++ Concentration/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Concentration
+//
+//  Created by tahori on 2019/08/21.
+//  Copyright © 2019 HORI Taisuke. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
